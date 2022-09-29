004bbd2eecb921a44645fa2ad0280b250ab13a16
dotrongkhang2000 <76832829+dotrongkhang2000@users.noreply.github.com>
2022-09-29 16:20:59 +0700

    [TOD-30] Add context menu to remove or edit workspace (#18)

---
+++ src/renderer/components/menus/menu-workspace.tsx
@@ -0,0 +1,81 @@
+import {
+  MenuItem,
+  ListItemText,
+  ListItemIcon,
+  Typography,
+  Menu,
+  Divider,
+} from "@mui/material";
+import { Delete, Edit } from "@mui/icons-material";
+// import DialogDelete from "../dialogs/dialog-delete-task";
+// import { useState } from "react";
+
+interface IMenuWorkspaceProps {
+  anchorEl: HTMLElement | null;
+  handleClose: () => void;
+  workspace: IWorkspace;
+}
+
+interface IMenuItem {
+  icon: React.ReactNode;
+  name: string;
+}
+
+const MenuWorkspace = ({
+  anchorEl,
+  handleClose,
+  workspace,
+}: IMenuWorkspaceProps) => {
+  // const [openDialogDeleteWorkspace, setOpenDialogDeleteWorkspace] =
+  //   useState(false);
+
+  const listMenuItem: IMenuItem[] = [
+    {
+      name: "Edit",
+      icon: <Edit fontSize="small" />,
+    },
+  ];
+
+  // const handleDeleteWorkspace = () => {
+  //   setOpenDialogDeleteWorkspace(true);
+  // };
+
+  return (
+    <Menu
+      sx={{ width: 320, maxWidth: "100%" }}
+      open={Boolean(anchorEl)}
+      onClose={() => handleClose()}
+      anchorEl={anchorEl}
+      anchorOrigin={{ horizontal: "center", vertical: "bottom" }}
+    >
+      {listMenuItem.map((menuItem) => (
+        <MenuItem key={menuItem.name}>
+          <ListItemIcon>{menuItem.icon}</ListItemIcon>
+          <ListItemText>
+            <Typography variant="body2">{menuItem.name}</Typography>
+          </ListItemText>
+        </MenuItem>
+      ))}
+      <Divider />
+      <MenuItem
+      // onClick={() => handleDeleteWorkspace()}
+      >
+        <ListItemIcon>
+          <Delete fontSize="small" />
+        </ListItemIcon>
+        <ListItemText>
+          <Typography variant="body2">Delete</Typography>
+        </ListItemText>
+      </MenuItem>
+
+      {/* <DialogDelete
+        open={openDialogDeleteWorkspace}
+        handleClose={() => setOpenDialogDeleteWorkspace(false)}
+        dialogName="workspace"
+        workspace={workspace}
+      /> */}
+    </Menu>
+  );
+};
+
+export default MenuWorkspace;
