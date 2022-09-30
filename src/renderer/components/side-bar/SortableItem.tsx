ed70db9fdde2ec52d16cb57c7e32d025c7891ac4
dotrongkhang2000 <76832829+dotrongkhang2000@users.noreply.github.com>
2022-09-30 14:18:31 +0700

    done

--- src/renderer/components/side-bar/SortableItem.tsx
+++ src/renderer/components/side-bar/SortableItem.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSortable } from '@dnd-kit/sortable';
 import { CSS } from '@dnd-kit/utilities';
 
 import { Box } from '@mui/material';
 import Workspace from './Workspace';
+import ContextMenuWorkspace from '../menus/context-menu-workspace';
 
 interface ISortableItemProps {
   workspaceId: string;
@@ -27,19 +28,34 @@ const SortableWorkspace = ({
     (workspace) => workspace.id === workspaceId
   );
 
+  const [anchorEl, setAnchorEL] = useState<null | HTMLElement>(null);
+
+  const handleClick = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    setAnchorEL(event.currentTarget);
+  };
+
   return (
-    <Box
-      sx={{
-        transform: CSS.Transform.toString(transform),
-        transition,
-        opacity: isDragging ? 0.5 : 1,
-      }}
-      ref={setNodeRef}
-      {...attributes}
-      {...listeners}
-    >
-      <Workspace workspace={workspaceRender!} />
-    </Box>
+    <>
+      <Box
+        sx={{
+          transform: CSS.Transform.toString(transform),
+          transition,
+          opacity: isDragging ? 0.5 : 1,
+        }}
+        ref={setNodeRef}
+        {...attributes}
+        {...listeners}
+        onContextMenu={(e) => handleClick(e)}
+      >
+        <Workspace workspace={workspaceRender!} />
+      </Box>
+
+      <ContextMenuWorkspace
+        anchorEl={anchorEl}
+        handleClose={() => setAnchorEL(null)}
+        workspace={workspaceRender!}
+      />
+    </>
   );
 };
 
