48837f7e034b859eece0c5a143f3877fab3f0274
dotrongkhang2000 <76832829+dotrongkhang2000@users.noreply.github.com>
2022-11-03 08:52:47 +0700

    done

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,13 +1,21 @@
-import React from 'react';
+/* eslint-disable @typescript-eslint/promise-function-async */
+import React, { Suspense } from 'react';
 
 import { CssBaseline } from '@mui/material';
-import { MainWindow } from '@/renderer/components/main-window';
+
+const MainWindow = React.lazy(() =>
+  import('@/renderer/components/main-window').then((module) => ({
+    default: module.MainWindow,
+  }))
+);
 
 export const App = () => {
   return (
     <>
       <CssBaseline />
-      <MainWindow />
+      <Suspense fallback={<div />}>
+        <MainWindow />
+      </Suspense>
     </>
   );
 };
